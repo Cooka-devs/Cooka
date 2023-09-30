19ebf2652d3c0eaced4a6ea885ea4fffe6d68283
Yoonhwi <ush0105@naver.com>
2023-09-30 21:59:35 +0900

    added modify/delete btn in comment

--- frontend/src/components/ShowComment/index.tsx
+++ frontend/src/components/ShowComment/index.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import { Comment } from "@/types";
+import { useState, useEffect } from "react";
+import { Comment, User } from "@/types";
 import Styles from "./index.module.css";
 import CommentsPageMove from "../CommentsPageMove";
+import { searchUser } from "@/api/getCurrentUser";
 interface ShowCommentProp {
   comments: Comment[];
 }
 const ShowComment = ({ comments }: ShowCommentProp) => {
   const [currentPage, setCurrentPage] = useState(1); //현재페이지
+  const [user, setUser] = useState<undefined | User>();
+  const [deleteComment, setDeleteComment] = useState<boolean>(false);
   const itemnum = 3; //페이지당 출력될 item 수
   const indexOfLast = currentPage * itemnum; //slice할때 마지막item 순서
   const indexOfFirst = indexOfLast - itemnum; // slice할때 첫item순서
@@ -16,6 +19,13 @@ const ShowComment = ({ comments }: ShowCommentProp) => {
     currentPosts = post.slice(indexOfFirst, indexOfLast);
     return currentPosts;
   };
+  useEffect(() => {
+    const fetch = async () => {
+      const getU = await searchUser();
+      setUser(getU);
+    };
+    fetch();
+  }, []);
   return (
     <>
       {CurrentPost(comments).map((comment) => (
@@ -24,6 +34,18 @@ const ShowComment = ({ comments }: ShowCommentProp) => {
             <div>{comment.writer}</div>
             <div>|</div>
             <div>{comment.created_at}</div>
+            {user && user?.nickname === comment.writer ? (
+              <>
+                <div>
+                  <button className={Styles.modify_btn}>삭제</button>
+                </div>
+                <div>
+                  <button className={Styles.modify_btn}>수정</button>
+                </div>
+              </>
+            ) : (
+              ""
+            )}
           </div>
           <div
             className={Styles.comment_comment}
