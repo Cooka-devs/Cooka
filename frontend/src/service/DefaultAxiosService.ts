86fbd3dc86ad81cd42981fd00ca757027a4914f9
Yoonhwi <ush0105@naver.com>
2023-11-06 14:41:24 +0900

    baseUrl 수정

--- frontend/src/service/DefaultAxiosService.ts
+++ frontend/src/service/DefaultAxiosService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class DefaultAxiosService {
   static instance = axios.create({
-    baseURL: `https://${process.env.NEXT_PUBLIC_SERVER_HOST}:8000`,
+    baseURL: `http://${process.env.NEXT_PUBLIC_SERVER_HOST}:8000`,
     withCredentials: true,
   });
 }
